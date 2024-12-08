94f113dbc23de0bf654de58d4ad642b6de218a3e
Highpower143 <nandyadu1c@gmail.com>
2024-12-08 22:38:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+OWNER_ID = int(getenv("OWNER_ID", "7166539540"))
+SUPPORT_GRP = "V2DDOS"
+UPDATE_CHNL = "CREATIVEYDV"
+OWNER_USERNAME = "TMZEROO"
     
